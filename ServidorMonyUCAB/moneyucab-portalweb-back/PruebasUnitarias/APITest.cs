dc8f78a12ccb1705ce374b593444f09f61736e5e
ezequiel <ezequielmonteroz8@gmail.com>
2020-07-23 04:50:23 -0800

    1. eliminado viejo proyecto Pruebas-Unitarias, 2. Creado nuevo proyecto PruebasUnitarias, 3. Agregado clase RegistroUsuario, Modelo Persona y APITest a PruebasUnitarias, 4. Agregada ultima version archivo Postman con las peticiones a la API

---
+++ ServidorMonyUCAB/moneyucab-portalweb-back/PruebasUnitarias/APITest.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using PruebasUnitarias.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasUnitarias
+{
+    static class APITest
+    {
+        static HttpClient client = new HttpClient();
+        static string url = "http://monyucab.somee.com";
+        static string token;
+
+        public static async Task<HttpResponseMessage> register(Persona persona)
+        {
+            var json = JsonConvert.SerializeObject(persona);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            return await client.PostAsync(url + "/api/authentication/register", data);
+        }
+    }
+}
