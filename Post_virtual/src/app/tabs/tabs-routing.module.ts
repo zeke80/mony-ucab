cc2a08fa5fed5b46fdc88c86114b4281e0f20d5f
jose romero <jaromero.16@est.ucab.edu.ve>
2020-07-24 16:07:54 -0400

    correccion de errores- se probo reintegro y funciona correctamente- se probo cobros exitosos funciona correctamente

--- Post_virtual/src/app/tabs/tabs-routing.module.ts
+++ Post_virtual/src/app/tabs/tabs-routing.module.ts
@@ -25,16 +25,16 @@ const routes: Routes = [
         path: 'operaciones',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
-      {
+      /*{
         path: 'operaciones/:operacionID',
         loadChildren: () => import('../tab3/operacion-detalle/operacion-detalle.module').then(m => m.OperacionDetallePageModule)
       },
       {
-        path: 'operaciones/monedero/:operacionID',
+        /*path: 'operaciones/monedero/:operacionID',
         loadChildren: () => import ('../tab3/operacion-detalle-m/operacion-detalle-m.module').then(m => m.OperacionDetalleMPageModule)
       },
       {
-        path: 'operaciones/tarjeta/:operacionID',
+       /* path: 'operaciones/tarjeta/:operacionID',
         loadChildren: () => import ('../tab3/operacion-detalle-t/operacion-detalle-t.module').then(m => m.OperacionDetalleTPageModule)
       },
       {
@@ -48,7 +48,7 @@ const routes: Routes = [
       {
         path: 'operaciones/cierre',
         loadChildren: () => import ('../tab3/cierre/cierre.module').then(m => m.CierrePageModule)
-      },
+      },*/
       {
         path: '',
         redirectTo: '/tabs/cuenta',
