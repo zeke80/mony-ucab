5fbe438dae5417aca8e461f73d871fa8463bc706
jose romero <jaromero.16@est.ucab.edu.ve>
2020-08-10 21:46:28 -0400

    modulo de pago con reintegro funcional y probado

--- Post_virtual/src/app/tab3/reintegroDetalle/reintegro-detalle/reintegro-detalle.page.ts
+++ Post_virtual/src/app/tab3/reintegroDetalle/reintegro-detalle/reintegro-detalle.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReintegroService } from 'src/app/servicios/reintegro/reintegro.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-reintegro-detalle',
@@ -21,7 +23,12 @@ export class ReintegroDetallePage implements OnInit {
     "referencia": ''
   }
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private reintegroService: ReintegroService,
+    private alert: AlertController,
+    private toastController: ToastController
+    ) { }
 
   setDetalle(){
    this.reintegroDetalle.idReintegro = localStorage.getItem('idReintegroDetalle');
@@ -38,7 +45,31 @@ getUser(){
   
 }
 pagoMonedero(){
-
+  var body = {
+    "idUsuarioReceptor":2,
+    "idMedioPaga":1,
+    "monto":100,
+    "idOperacion":2
+  }
+  
+  body.idUsuarioReceptor = parseInt(localStorage.getItem('idUsuarioSolicitanteDetalle'));
+  body.idMedioPaga = parseInt(localStorage.getItem('idUsuario'));
+  body.monto = parseInt(localStorage.getItem('montoDetalle'));
+  body.idOperacion  = parseInt(localStorage.getItem('idReintegroDetalle'));
+  
+  this.reintegroService.pagarReintegroMonedero(body).
+  subscribe(
+    (data: any) =>
+    {
+      this.successToast('success', 'reintegro realizado satisfactoriamente');
+      console.log(data);
+      this.router.navigate(['/tabs/cuenta']);
+    },
+    err =>{
+      console.log(err);
+      
+      this.dangerToast('danger', 'ERROR! reintegro no realizado ');    }
+  )
 }
 
 pagoPaypal(){
@@ -47,6 +78,34 @@ pagoPaypal(){
 
 pagoStripe(){
   this.router.navigate(['tabs/operaciones/pago']);
+}
+
+async successToast(color : string, mensaje : string) {
+  const success = await this.toastController.create({
+    message: mensaje,
+    color : color,
+    buttons: [ 
+      {
+        icon: 'close',
+        role: 'cancel'
+      }
+    ]
+  });
+  success.present();
+}
+
+async dangerToast(color : string, mensaje : string) {
+  const success = await this.toastController.create({
+    message: mensaje,
+    color : color,
+    buttons: [ 
+      {
+        icon: 'close',
+        role: 'cancel'
+      }
+    ]
+  });
+  success.present();
 }
 
   ngOnInit() {
