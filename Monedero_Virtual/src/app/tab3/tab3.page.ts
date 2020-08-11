1753b2a4faa5223b5aa700a66cfdd524d0e44190
Luis Da Silva <paliyon1@gmail.com>
2020-08-11 14:57:44 -0400

    Arreglo historial cuentas y tarjetas
    Ahora se muestran todas las tarjetas y todas las cuentas que tenga el usuario

--- Monedero_Virtual/src/app/tab3/tab3.page.ts
+++ Monedero_Virtual/src/app/tab3/tab3.page.ts
@@ -5,6 +5,10 @@ import { Usuario } from '../models/usuario.model';
 import { UsuarioService } from '../servicios/usuario/usuario.service';
 import { CuentaService } from '../servicios/cuenta/cuenta.service';
 import { TarjetaService } from '../servicios/tarjeta/tarjeta.service';
+import { Platform, NavController, NavParams } from '@ionic/angular';
+//import {  } from 'ionic-angular';
+//import {  } from 'ionic-angular';
+//import { type } from 'os';
 
 @Component({
   selector: 'app-tab3',
@@ -14,12 +18,16 @@ import { TarjetaService } from '../servicios/tarjeta/tarjeta.service';
 export class Tab3Page implements OnInit{
 
   cuentas = [];
+  ocuentas = [];
   operaciones = [];
   tarjetas = [];
+  otarjetas = [];
   operacionesTarjeta = [];
   monederos = [];
   reintegros = [];
   usuario: Usuario
+  public navParams = new NavParams();
+  aux = false;
 
   constructor(
     public _tarjetaService: TarjetaService,
@@ -28,40 +36,30 @@ export class Tab3Page implements OnInit{
     public router: Router,
     public _usuarioServices: UsuarioService,
     private _activatedRoute: ActivatedRoute,
+    public navCtrl: NavController
 
   ) {
     this._activatedRoute.paramMap.subscribe(params => {
       this.ngOnInit();
   });
   }
+  
 
   ngOnInit(){
-    this.operaciones = this._operacionServices.getoperacionesCuentaVacio();
-    this.tarjetas = this._operacionServices.getoperacionesTarjetaVacio();
-    this.monederos = this._operacionServices.getoperacionesMonederoVacio();
-    this.reintegros = this._operacionServices.getreintegrosVacio();
     this.usuario = this._usuarioServices.getUsuario();
+    //this.operaciones = this._operacionServices.getoperacionesCuentaVacio();
+  
+    //this.monederos = this._operacionServices.getoperacionesMonederoVacio();
+    this.reintegros = this._operacionServices.getreintegrosVacio();
+   
+    if(this.aux){
     this.getAccountsAndLoadOperations();
     this.loadMonederoperations();
-    this.getTargetsAndLoadTargetOperations();
+    //this.getTargetsAndLoadTargetOperations();
+    this.loadTargetOperations();
+    this.loadAccountsOperations();
     }
-
-    loadTargetOperations(){
-      this.tarjetas.forEach((tarjeta)=>{
-        this._operacionServices.getoperacionesTarjeta(tarjeta.idTarjeta)
-        .toPromise().then((data:any)=>{
-          this.operacionesTarjeta = data
-        }  )
-        .catch(()=>console.log('Usurio no posee tarjetas.'));
-      });
-    }
-
-    getTargetsAndLoadTargetOperations(){
-      console.log(this.usuario.idUsuario);
-      this._tarjetaService.obtenerTarjetas(this.usuario.idUsuario)
-      .toPromise().then((data: any) => {
-        this.tarjetas = data;
-        }).then(()=>this.loadTargetOperations());
+    this.aux=true;
     }
 
     loadMonederoperations(){
@@ -69,8 +67,7 @@ export class Tab3Page implements OnInit{
       .toPromise().then((data: any )=>{
         this.monederos = data;
       }).then(()=>this.cleanMonederoOperations());
-    }
-
+    } //carga de operaciones monedero
     isNotClosingOperation(monedero){
       return monedero.infoAdicional.tipoOperacion.idTipoOperacion !== 4;
     }
@@ -79,6 +76,50 @@ export class Tab3Page implements OnInit{
       this.monederos = this.monederos.filter(this.isNotClosingOperation);
     }
 
+    loadTargetOperations(){
+      this.tarjetas = [];
+      this.otarjetas = [];
+      this._tarjetaService.obtenerTarjetas(this.usuario.idUsuario)
+          .subscribe((data: any) => {
+            this.tarjetas = data;
+            this.tarjetas.forEach((tarjeta)=>{
+              this._operacionServices.getoperacionesTarjeta(tarjeta.idTarjeta).subscribe(
+                (data: any) => { 
+                  for (let opp of data) {
+                    this.otarjetas.push(opp);
+                  }   
+                },
+                error => {
+                 console.log("Error en subscribe")
+                });
+            }); 
+          });
+    }
+
+    public test(event , otarjeta ){
+      // this.navCtrl. push(page2,{
+      // item:item
+      // });
+      this.router.navigate(['/tabs/operaciones/tarjeta/', { idTarjeta: otarjeta['idTarjeta'], idOperacionTarjeta: otarjeta['idOperacionTarjeta']}])
+    } // pasar informacion de una page a otra
+
+    loadAccountsOperations(){
+      this.cuentas = [];
+      this.ocuentas = [];
+      this._cuentaServices.obtenerCuenta(this.usuario.idUsuario).subscribe((data:any) =>{
+          this.cuentas = data;
+          console.log("cuentas",this.cuentas);
+          this.cuentas.forEach((cuenta)=>{
+            this._operacionServices.getoperacionesCuenta(cuenta._idCuenta).subscribe((data:any) =>{
+            for (let opp of data){
+              this.ocuentas.push(opp);
+            }
+            });
+          });
+          console.log("Operaciones Cuentas: ",this.ocuentas);
+      });
+    }
+    
     getAccountsAndLoadOperations(){
       this._cuentaServices.obtenerCuenta(this.usuario.idUsuario)
       .toPromise().then((data: any) => {
